a32ae0421d5a90d52fdcdccae2b16b5a9bdb2d39
lili <lili219114@sohu-inc.com>
2023-04-08 18:16:15 +0800

    feat: 🎸 react-dom build2

---
+++ scripts/rollup/react-dom.config.js
@@ -0,0 +1,48 @@
+import { resolvePkgPath, getPackageJSON, getBaseRollupPlugins } from './utils';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import alias from '@rollup/plugin-alias';
+
+const { name, module } = getPackageJSON('react-dom');
+const pkgPath = resolvePkgPath(name);
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: [
+			{
+				file: `${pkgDistPath}/index.js`,
+				name: 'index.js',
+				format: 'umd'
+			},
+			{
+				file: `${pkgDistPath}/client.js`,
+				name: 'client.js',
+				format: 'umd'
+			}
+		],
+		plugins: [
+			...getBaseRollupPlugins(),
+			// webpack resolve alias
+			alias({
+				entries: {
+					hostConfig: `${pkgPath}/src/hostConfig.ts`
+				}
+			}),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, version, description }) => ({
+					name,
+					version,
+					description,
+					peerDependencies: {
+						react: version
+					},
+					main: 'index.js'
+				})
+			})
+		]
+	}
+];
