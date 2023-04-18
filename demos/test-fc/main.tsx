04c4aef9da0072f75ed3f81ecb5abf7623f35995
lili <lili219114@sohu-inc.com>
2023-04-18 18:01:15 +0800

    feat: 🎸 修改demo和tsconfig

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { ReactElementType } from 'shared/ReactTypes';
 
 const App = () => {
 	return (
@@ -13,9 +13,5 @@ const A = () => {
 	return <h3>hehe</h3>;
 };
 
-console.log(React);
-console.log(ReactDOM);
-console.log(App);
-
 const root = document.querySelector('#root') as Element;
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(root).render((<App />) as ReactElementType);
--- demos/test-useState/main.tsx
+++ demos/test-useState/main.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { ReactElementType } from 'shared/ReactTypes';
 
 const App = () => {
+	return (
+		<div>
+			<A></A>
+		</div>
+	);
+};
+
+const A = () => {
 	const [num, setNum] = useState(100);
-	window.setNum = setNum;
-	return <div>{num}</div>;
+	return (
+		<h3 onClick={() => setNum(num + 1)}>{num % 2 === 0 ? 'hehe' : num}</h3>
+	);
 };
 
 const root = document.querySelector('#root') as Element;
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(root).render((<App />) as ReactElementType);
