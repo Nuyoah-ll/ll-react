8d2ee9234ea5ff20e674527e8b895fbb323d48e5
lili <lili219114@sohu-inc.com>
2023-05-22 19:03:43 +0800

    feat: 🎸 完成多节点diff

--- packages/react-reconciler/src/childFibers.ts
+++ packages/react-reconciler/src/childFibers.ts
@@ -8,6 +8,8 @@ import {
 import { ChildDeletion, Placement } from './fiberFlags';
 import { HostText } from './workTags';
 
+type ExistingChildren = Map<string | number, FiberNode>;
+
 function ChildReconciler(shouldTrackEffects: boolean) {
 	function deleteChild(returnFiber: FiberNode, childToDelete: FiberNode) {
 		if (!shouldTrackEffects) {
@@ -113,6 +115,136 @@ function ChildReconciler(shouldTrackEffects: boolean) {
 		return fiber;
 	}
 
+	// 在原版react中，newChild的类型就是any，虽然当前我们只用考虑reactElement的情况，但是实际上react中还有很多其他类型
+	function reconcileChildrenArray(
+		returnFiber: FiberNode,
+		currentFirstChild: FiberNode | null,
+		newChild: any[]
+	) {
+		// 最后一个可复用的fiber在current fiber中的位置
+		let lastPlacedIndex = 0;
+		// 创建的最后一个fiber，对应的updateFromMap的返回值
+		let lastNewFiber: FiberNode | null = null;
+		// 创建的第一个fiber，也就是reconcileChildrenArray函数返回的下一个wip fiber
+		let firstNewFiber: FiberNode | null = null;
+
+		// 1. 将current保存在map中
+		const existingChildren: ExistingChildren = new Map();
+		let current = currentFirstChild;
+		while (current !== null) {
+			//! 这里要仔细思考，可能涉及到为什么key值不能用index
+			const keyToUse = current.key !== null ? current.key : current.index;
+			existingChildren.set(keyToUse, current);
+			current = current.sibling;
+		}
+
+		// 当遍历element时，「当前遍历到的element」一定是「所有已遍历的element」中最靠右那个。
+		for (let i = 0; i < newChild.length; i++) {
+			// 2. 遍历new Child，寻找是否可复用
+			const after = newChild[i];
+			const newFiber = updateFromMap(returnFiber, existingChildren, i, after);
+			if (newFiber === null) {
+				continue;
+			}
+			// 3. 标记移动或者插入
+			// 移动的判断依据：element的index与「element复用的current fiber」的index的比较
+			newFiber.index = i;
+			newFiber.return = returnFiber;
+
+			if (lastNewFiber === null) {
+				lastNewFiber = newFiber;
+				firstNewFiber = newFiber;
+			} else {
+				lastNewFiber.sibling = newFiber;
+				lastNewFiber = lastNewFiber.sibling;
+			}
+
+			if (!shouldTrackEffects) {
+				continue;
+			}
+
+			const current = newFiber.alternate;
+			if (current !== null) {
+				const oldIndex = current.index;
+				if (oldIndex < lastPlacedIndex) {
+					// 移动
+					newFiber.flags |= Placement;
+					continue;
+				} else {
+					// 不移动
+					lastPlacedIndex = oldIndex;
+				}
+			} else {
+				// fiber不能复用，插入新节点
+				newFiber.flags |= Placement;
+			}
+		}
+
+		// 4. 将map中剩下的标记为删除
+		existingChildren.forEach((fiber) => {
+			deleteChild(returnFiber, fiber);
+		});
+		return firstNewFiber;
+	}
+
+	// 返回fiberNode代表可以复用或者是新建的fiber
+	function updateFromMap(
+		returnFiber: FiberNode,
+		existingChildren: ExistingChildren,
+		index: number,
+		element: any
+	): FiberNode | null {
+		const keyToUse = element.key !== null ? element.key : index;
+		const before = existingChildren.get(keyToUse);
+
+		// HostText
+		if (typeof element === 'string' || typeof element === 'number') {
+			if (before) {
+				if (before.tag === HostText) {
+					existingChildren.delete(keyToUse);
+					return useFiber(before, { content: element + '' });
+				}
+			}
+			return new FiberNode(HostText, { content: element + '' }, null);
+		}
+
+		// ReactElement
+		if (typeof element === 'object' && element !== null) {
+			switch (element.$$typeof) {
+				case REACT_ELEMENT_TYPE:
+					if (before) {
+						if (before.type === element.type) {
+							existingChildren.delete(keyToUse);
+							return useFiber(before, element.props);
+						}
+					}
+					return createFiberFromElement(element);
+			}
+
+			if (Array.isArray(element) && __DEV__) {
+				console.warn('还未实现数组类型的child');
+			}
+		}
+
+		return null;
+
+		// TODO element是数组或者是Fragment的情况，
+		// <ul>
+		// 	<li/>
+		// 	<li/>
+		// 	{[<li/>, <li/>]}
+		// </ul>
+
+		// <ul>
+		// 	<li/>
+		// 	<li/>
+		// 	<>
+		//     <li/>
+		//     <li/>
+		// 	</>
+		// </ul>
+	}
+
 	// returnFiber代表当前正在处理的wip fiber，currentFiber代表其“子” current fiber，newChild代表其“子” reactElement
 	return function (
 		returnFiber: FiberNode,
@@ -121,7 +253,6 @@ function ChildReconciler(shouldTrackEffects: boolean) {
 	) {
 		// 判断当前fiber的类型
 		// 单节点
-		//? q 如果newChild是多节点，那么它是一个数组，难道不会走这个单节点逻辑？
 		if (typeof newChild === 'object' && newChild !== null) {
 			switch (newChild.$$typeof) {
 				case REACT_ELEMENT_TYPE:
@@ -132,6 +263,11 @@ function ChildReconciler(shouldTrackEffects: boolean) {
 					if (__DEV__) {
 						console.warn('未实现的reconcile类型', newChild);
 					}
+			}
+
+			//? 写错了，这个为啥不写外面，虽然看逻辑写在这里也是没问题的？
+			if (Array.isArray(newChild)) {
+				return reconcileChildrenArray(returnFiber, currentFiber, newChild);
 			}
 		}
 
